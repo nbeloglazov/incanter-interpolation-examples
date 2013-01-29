347e196bb7f8559e6fca42154c84fb18e05bec68
Nikita Beloglazov <nikelandjelo@gmail.com>
2013-01-29 16:15:17 +0300

    Add initial project for surface interpolation.

---
+++ surfaces/install_libs.bat
@@ -0,0 +1,2 @@
+lein localrepo install libs/hemesh.jar wblut/hemesh 1.8
+lein localrepo install libs/quickhull3d.jar quickhull3d/quickhull3d 1.0
---
+++ surfaces/project.clj
@@ -0,0 +1,8 @@
+(defproject surfaces "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.apfloat/apfloat "1.6.3"]
+                 [javolution "5.5.1"]
+                 [com.vividsolutions/jts "1.13"]
+                 [quickhull3d "1.0"]
+                 [wblut/hemesh "1.8"]]
+  :plugins [[lein-localrepo "0.4.1"]])
