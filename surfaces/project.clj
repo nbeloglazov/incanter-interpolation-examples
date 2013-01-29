f955e199125eed0683e67fb7bad519a624bef5ad
Nikita Beloglazov <nikelandjelo@gmail.com>
2013-01-29 16:17:42 +0300

    Add quil dependency.

--- surfaces/project.clj
+++ surfaces/project.clj
@@ -4,5 +4,6 @@
                  [javolution "5.5.1"]
                  [com.vividsolutions/jts "1.13"]
                  [quickhull3d "1.0"]
-                 [wblut/hemesh "1.8"]]
+                 [wblut/hemesh "1.8"]
+                 [quil "1.6.0"]]
   :plugins [[lein-localrepo "0.4.1"]])
