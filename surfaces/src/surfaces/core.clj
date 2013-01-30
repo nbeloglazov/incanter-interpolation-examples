260615b420394c3ebbe361a571775a1e1eadc114
Nikita Beloglazov <nikelandjelo@gmail.com>
2013-01-30 16:57:28 +0300

    Show surface of 1x1x1 size.

--- surfaces/src/surfaces/core.clj
+++ surfaces/src/surfaces/core.clj
@@ -5,41 +5,51 @@
 (def mesh (atom nil))
 (def render (atom render))
 
-(defn create-mesh []
-  (let [creator (wblut.hemesh.HEC_Cube. 300 1 1 1)
-        m (wblut.hemesh.HE_Mesh. creator)]
-    (.modify m
-             (doto (wblut.hemesh.HEM_Extrude.)
-               (.setDistance 300.0)))
-    (reset! mesh
-            (wblut.hemesh.HE_Mesh.
-             (doto (wblut.hemesh.HEC_FromFrame.)
-               (.setFrame m)
-               (.setMaximumStrutLength 20))))))
+(defn create-surface-mesh [f [u-l u-u] [v-l v-u]]
+  (let [surface (proxy [wblut.geom.WB_Surface] []
+                  (loweru [] u-l)
+                  (lowerv [] v-l)
+                  (upperu [] u-u)
+                  (upperv [] v-u)
+                  (surfacePoint [u v]
+                    (let [[x y z] (f u v)]
+                      (wblut.geom.WB_Point3d. (double x) (double y) (double z)))))]
+    (wblut.hemesh.HE_Mesh. (wblut.hemesh.HEC_FromSurface. surface 100 100 false false))))
+
+
+(defn f [u v]
+  (let [r (+ 0.5 (/ u TWO-PI))]
+    [(* (cos v) (cos u) r)
+     (* (cos v) (sin u) r)
+     (* (sin v) r)]))
 
 (defn setup []
-  (create-mesh)
-  (let [modifier (wblut.hemesh.HEM_SphereInversion.)]
-    (doto modifier
-      (.setRadius 200)
-      (.setCenter 50 0 0)
-      (.setCutoff 1000)
-      (.setLinear false))
-    (.modify @mesh modifier))
+  (reset! mesh (create-surface-mesh f [0 TWO-PI] [(- PI) PI]))
   (reset! render (wblut.processing.WB_Render. (current-applet))))
 
 (defn draw []
+  (lights)
   (background 120)
-  (directional-light 255 255 255 1 1 -1)
-  (directional-light 127 127 127 -1 -1 1)
-  (translate 400 400 0)
+  (push-matrix)
+  (translate 400 400)
   (rotate-y (* (mouse-x) (/ 1.0 (width)) TWO-PI))
   (rotate-x (* (mouse-y) (/ 1.0 (height)) TWO-PI))
+  (scale 100)
   (fill 255)
   (no-stroke)
   (.drawFaces @render @mesh)
   (stroke 0)
-  (.drawEdges @render @mesh))
+  (.drawEdges @render @mesh)
+  #_(do (stroke-weight 10)
+      (stroke 255 0 0)
+      (line 0 0 0 1 0 0)
+      (stroke 0 255 0)
+      (line 0 0 0 0 1 0)
+      (stroke 0 0 255)
+      (line 0 0 0 0 0 1)
+      (stroke-weight 1))
+  
+  (pop-matrix))
 
 
 (defsketch example                  ;;Define a new sketch named example
